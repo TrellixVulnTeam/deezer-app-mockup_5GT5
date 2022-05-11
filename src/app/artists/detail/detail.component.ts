e74a902ce6560fabcc554671002c44255ad08293
Norman Munge <normanmunge@gmail.com>
2022-05-11 09:46:11 +0300

    Adds logic to get artist details based on id

--- src/app/artists/detail/detail.component.ts
+++ src/app/artists/detail/detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common'; //-> use to navigate back to the view that navigated here
 
+import { ArtistService } from 'src/app/services/artist/artist.service';
+import { Artist } from 'src/app/interfaces/artist-details';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  artist: any = null;
+  isBanner: boolean = true;
+
+  constructor(
+    private route: ActivatedRoute,
+    private artistService: ArtistService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.getArtistDetails();
+  }
+
+  getArtistDetails():void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.artistService.getArtist(id).subscribe(artist => {
+      console.log('THE ARTIST DETAILS', artist);
+      this.artist = artist
+    });
+  }
+
+  navigateBack():void {
+    this.location.back();
   }
 
 }

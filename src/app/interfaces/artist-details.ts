b569482bea926e6ff63f4dada8cc2ceddd12f163
Norman Munge <normanmunge@gmail.com>
2022-05-19 09:08:03 +0300

    Updates artist model

--- src/app/interfaces/artist-details.ts
+++ src/app/interfaces/artist-details.ts
@@ -5,7 +5,13 @@ export interface Artist {
   id: number;
   name: string;
   album: Album[];
-  fansCount: number;
-  artwork: any;
+  nb_fan: number;
+  picture: any;
+  picture_big: any;
+  picture_medium: any;
+  picture_small: any;
+  picture_xl: any;
+  tracklist: any;
+  nb_album: number;
   tracks: Track[];
 }

7efc2e3a45a2ae7d421e72342520ec362ceecce8
Norman Munge <normanmunge@gmail.com>
2022-05-20 12:12:26 +0300

    Artist details fetched from api based on id

--- src/app/interfaces/artist-details.ts
+++ src/app/interfaces/artist-details.ts
@@ -1,19 +1,20 @@
 import { Album } from './album';
 import { Track } from './track';
 
+export interface Artists {
+  data: Array<Artist>;
+}
 export interface Artist {
-  data: {
-    id: number;
-    name: string;
-    album: Album[];
-    nb_fan: number;
-    picture: any;
-    picture_big: any;
-    picture_medium: any;
-    picture_small: any;
-    picture_xl: any;
-    tracklist: any;
-    nb_album: number;
-    tracks: Track[];
-  };
+  id?: number;
+  name?: string;
+  album?: Album[];
+  nb_fan?: number;
+  picture?: any;
+  picture_big?: any;
+  picture_medium?: any;
+  picture_small?: any;
+  picture_xl?: any;
+  tracklist?: any;
+  nb_album?: number;
+  tracks?: Track[];
 }

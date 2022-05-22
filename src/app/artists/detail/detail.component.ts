1427eade9da1d87c41631de83b354aea03901d66
Norman Munge <normanmunge@gmail.com>
2022-05-22 03:06:08 +0300

    Style updates on the artist detail page

--- src/app/artists/detail/detail.component.ts
+++ src/app/artists/detail/detail.component.ts
@@ -7,34 +7,32 @@ import { Artist } from 'src/app/interfaces/artist-details';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.scss']
+  styleUrls: ['./detail.component.scss'],
 })
 export class DetailComponent implements OnInit {
-
-  artist: any = null;
+  artist: Artist = {};
   isBanner: boolean = true;
 
   constructor(
     private route: ActivatedRoute,
     private artistService: ArtistService,
     private location: Location
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.getArtistDetails();
   }
 
-  getArtistDetails():void {
+  getArtistDetails(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
 
-    this.artistService.getArtist(id).subscribe(artist => {
-      console.log('THE ARTIST DETAILS', artist);
-      this.artist = artist
+    this.artistService.getArtist(id).subscribe((res: any) => {
+      console.log('THE ARTIST DETAILS', res);
+      this.artist = res.data;
     });
   }
 
-  navigateBack():void {
+  navigateBack(): void {
     this.location.back();
   }
-
 }

8fa7f2bed4199a9b21aedb94e17b84d07aea1f79
Norman Munge <normanmunge@gmail.com>
2022-05-22 03:07:39 +0300

    Updates styling of track item componet

--- src/app/shareable/track-item/track-item.component.ts
+++ src/app/shareable/track-item/track-item.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-track-item',
   templateUrl: './track-item.component.html',
-  styleUrls: ['./track-item.component.scss']
+  styleUrls: ['./track-item.component.scss'],
 })
 export class TrackItemComponent implements OnInit {
+  @Input() isHeader: any;
+  @Input() item: any;
+  @Input() isIndex: number = 1;
+  @Input() isLast: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    // console.log('THE TRACK ITEM', this.item);
   }
-
 }
